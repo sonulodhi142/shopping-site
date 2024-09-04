d293c8883c07adb1ba1bf21528d875e0daff5dbc
Sonu Lodhi <sonulodhis142@gmail.com>
2024-09-04 11:13:59 +0530

    install redux toolkit

--- shopclone/src/App.js
+++ shopclone/src/App.js
@@ -1,19 +1,12 @@
 
 import './App.css';
-import Collapes from './components/Collapes';
-import Grid from './components/Grid';
-import Header from './components/header';
-import Table from './components/Table';
-import Alert from './Alert';
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Grid/>
-      <Table/>
-      <Collapes/>
-      <Alert/>
+      <h1>jai shri ram</h1>
+      
     </div>
   );
 }
